34c0e6beee80aa5d3429e00d178c3b0f3d7cf859
Nick <nicklandkamer@gmail.com>
2022-04-20 15:32:03 -0500

    fix: menu restyled (#17)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'mfdb-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() addFriend: EventEmitter<void> = new EventEmitter();
+  routes: any[] = [
+    { path: ['/'], text: 'Home' },
+    { path: ['/', 'friend-list'], text: 'Friends List' },
+    { path: ['/', 'charts'], text: 'Charts' },
+  ];
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,7 +6,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules = [
   MatButtonModule,
@@ -17,6 +19,8 @@ const modules = [
   MatSelectModule,
   MatIconModule,
   MatDialogModule,
+  MatToolbarModule,
+  MatMenuModule,
 ];
 
 @NgModule({
